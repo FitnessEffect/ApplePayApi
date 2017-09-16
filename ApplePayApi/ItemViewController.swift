59d5bc8d868d1c73cb4e13a8cd1e7866a99b9d58
Stefan Auvergne <stefan.auvergne@gmail.com>
2017-09-16 13:06:59 -0400

    customize pay sheet (shipping + address only)

--- ApplePayApi/ItemViewController.swift
+++ ApplePayApi/ItemViewController.swift
@@ -11,7 +11,26 @@ import PassKit
 
 class ItemViewController: UIViewController {
     
+    struct ShippingMethod {
+        let price: NSDecimalNumber
+        let title: String
+        let description: String
+        
+        init(price: NSDecimalNumber, title: String, description: String) {
+            self.price = price
+            self.title = title
+            self.description = description
+        }
+        
+        static let ShippingMethodOptions = [
+            ShippingMethod(price: NSDecimalNumber(string: "5.00"), title: "Carrier Pigeon", description: "You'll get it someday."),
+            ShippingMethod(price: NSDecimalNumber(string: "100.00"), title: "Racecar", description: "Vrrrroom! Get it by tomorrow!"),
+            ShippingMethod(price: NSDecimalNumber(string: "9000000.00"), title: "Rocket Ship", description: "Look out your window!"),
+            ]
+    }
+    
     var cellNumber = 0
+    let shippingPrice: NSDecimalNumber = NSDecimalNumber(string: "5.0")
 
     @IBOutlet weak var item: UILabel!
     @IBOutlet weak var applePayButton: UIButton!
@@ -43,10 +62,13 @@ class ItemViewController: UIViewController {
         request.countryCode = "US"
         request.currencyCode = "USD"
         request.paymentSummaryItems = [
-            PKPaymentSummaryItem(label: "user", amount: NSDecimalNumber(integerLiteral: cellNumber)),
-            PKPaymentSummaryItem(label: "Stefan", amount: NSDecimalNumber(integerLiteral: cellNumber))
+            PKPaymentSummaryItem(label: "Item", amount: NSDecimalNumber(integerLiteral: cellNumber)),
+            PKPaymentSummaryItem(label: "Shipping", amount: shippingPrice),
+            PKPaymentSummaryItem(label: "Stefan", amount: NSDecimalNumber(integerLiteral: (cellNumber + Int(shippingPrice))))
         ]
+        request.requiredShippingAddressFields = PKAddressField.postalAddress
         let applePayController = PKPaymentAuthorizationViewController(paymentRequest: request)
+        applePayController.delegate = self
         self.present(applePayController, animated: true, completion: nil)
     }
 
@@ -60,4 +82,14 @@ class ItemViewController: UIViewController {
     }
     */
 
+}
+
+extension ItemViewController: PKPaymentAuthorizationViewControllerDelegate {
+    func paymentAuthorizationViewController(_ controller: PKPaymentAuthorizationViewController, didAuthorizePayment payment: PKPayment, completion: @escaping ((PKPaymentAuthorizationStatus) -> Void)) {
+        completion(PKPaymentAuthorizationStatus.success)
+    }
+    
+    func paymentAuthorizationViewControllerDidFinish(_ controller: PKPaymentAuthorizationViewController) {
+        controller.dismiss(animated: true, completion: nil)
+    }
 }
