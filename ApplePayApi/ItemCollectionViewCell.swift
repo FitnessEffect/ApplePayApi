623e8591b8196a4a4ca9fa1571752cf85703dad0
Stefan Auvergne <stefan.auvergne@gmail.com>
2017-09-16 11:08:24 -0400

    collection view

---
+++ ApplePayApi/ItemCollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  ItemCollectionViewCell.swift
+//  ApplePayApi
+//
+//  Created by Stefan Auvergne on 9/16/17.
+//  Copyright © 2017 com.example. All rights reserved.
+//
+
+import UIKit
+
+class ItemCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var itemLabel: UILabel!
+    
+}
